6b919caab00654f391d6009fb8d0c2ba941b8475
Achinta <achintadutta1997@gmail.com>
2025-07-10 12:27:14 +0530

    new rg added

--- child/rg/rg.tf
+++ child/rg/rg.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "RG1" {
   name     = "az-rg1"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "RG2" {
+  name     = "az-rg2"
+  location = "West Europe"
+}
